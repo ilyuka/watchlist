a3d8ffbefd94c1065be7474d1a830d73c1a02ae6
ilyuka <theilyashulepov@gmail.com>
2024-06-19 12:40:37 +0300

    remove services/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import Link from "next/link";
-import { getAllLists } from "@/services/listService";
-import SearchField from "@/components/SearchField/SearchField";
-
 export default async function Home() {
-    const allLists = await getAllLists();
-
-    return (
-        <main>
-            <Link href="/list/create">Create New List</Link>
-            <div>
-                <ul>
-                    {allLists?.map((list, index) => {
-                        return <li key={index}>{list.title}</li>;
-                    })}
-                </ul>
-            </div>
-            <div>{/* <SearchField></SearchField> */}</div>
-        </main>
-    );
+    return <main></main>;
 }
